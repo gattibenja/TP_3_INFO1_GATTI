8c5a066c1aa1f3f823edf54186fab175a23a4066
gattibenja <120605899+gattibenja@users.noreply.github.com>
2025-05-17 14:33:45 -0300

    Add files via upload

---
+++ bmi.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+float peso, altura, indice;
+
+int main() {
+	printf("Ingrese su peso corporal:\n");
+	scanf("%f", &peso);
+	printf("Ingrese su altura:\n");
+	scanf("%f", &altura);
+	
+	indice = (peso / (altura * altura));
+	
+	printf("Su indice de masa corporal es:\n");
+	printf("%02.2f", indice);
+	
+	printf("\n\nIndice | Condición \n ----------------------------- \n <18.5 | Bajo peso \n 18.5 a 24.9 | Normal \n 25.0 a 29.9 | Sobrepeso \n >=30| Obesidad");
+	
+	return 0;
+ }
+
